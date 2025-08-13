900967567c6c223af869f5658858dfc59721ecc4
Franco Huarte <Franconicolashuarte@hotmail.com>
2025-08-13 16:13:53 -0300

    Primeros cambios, imagenes y descripciones dinamicas, como asi tambien la creacion de un nuevo componente

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,54 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png'
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  )
+}
+
+function CoreConcepts() {
+  return
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core UI builiding block"
+              image={componentsImg}
+            />
+            <CoreConcepts />
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
