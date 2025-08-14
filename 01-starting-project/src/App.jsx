45de4a5cb81db9793cc30d8c3e1876f616589707
Franco Huarte <Franconicolashuarte@hotmail.com>
2025-08-14 11:31:51 -0300

    Segundos cambios, se separo los componentes y sus respectivos estilos en archivos diferentes del de App.jsx dando mas estructuracion al proyecto

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,10 @@
-import reactImg from './assets/react-core-concepts.png';
+
 import { CORE_CONCEPTS } from './data.js'; //Aca importo los datos desde data
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcepts({image, title, description}) {
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
 
 function App() {
   return (
@@ -41,16 +14,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
+            <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} /> //Es lo mismo pero de manera abrevida los '...' agregan todo los items que vemos arriba
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} /> {//Es lo mismo pero de manera abrevida los '...' agregan todo los items que vemos arriba
+            <CoreConcept {...CORE_CONCEPTS[2]} /> }
+            <CoreConcept {...CORE_CONCEPTS[3]} />                                               
           </ul>
-
         </section>
       </main>
     </div>
